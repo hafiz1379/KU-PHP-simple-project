985a7b9a27ac8c1ffe1f90d27eac4530501f5619
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-06 12:20:22 +0430

    Add user information management page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Information</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>User Information Management</h1>
+    <ul>
+        <li><a href="user_form.html">Add User</a></li>
+        <li><a href="display_users.php">View Users</a></li>
+    </ul>
+</body>
+</html>
